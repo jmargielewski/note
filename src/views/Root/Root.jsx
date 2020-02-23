0a207e91f5c020856c9d58e55dd0190e35575d93
jmargielewski <kuba.margielewski@gmail.com>
2020-02-23 22:46:30 +0000

    add styled components

---
+++ src/views/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+function Root() {
+  return (
+    <div className="Root">
+      <Button>Hello</Button>
+      <Button secondary>Hello</Button>
+    </div>
+  );
+}
+
+export default Root;
