ff4930c48da080ef1b84f3ba0a44e48ae0c81e01
jmargielewski <kuba.margielewski@gmail.com>
2020-03-01 09:05:06 +0000

    update theme

--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -8,6 +8,15 @@ const theme = {
 
   light: 300,
   bold: 600,
+
+  fontSize: {
+    xxs: '1rem',
+    xs: '1.2rem',
+    s: '1.6rem',
+    m: '2.1rem',
+    l: '2.4rem',
+    xl: '4rem',
+  },
 };
 
 export default theme;
