a850b0c769a31d132cd1de090a6b366fbe89099d
jmargielewski <kuba.margielewski@gmail.com>
2020-03-21 12:46:34 +0000

    update templates and add context

--- src/templates/DetailsTemplate.jsx
+++ src/templates/DetailsTemplate.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
-// import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import UserPageTemplate from './UserPageTemplate';
 
-const DetailsTemplate = ({ children }) => {
-  return (
-    <UserPageTemplate>
-      {children}
-      <Link to="/">go back</Link>
-    </UserPageTemplate>
-  );
+const DetailsTemplate = ({ children, pageType }) => {
+  return <UserPageTemplate pageType={pageType}>{children}</UserPageTemplate>;
 };
 
 DetailsTemplate.propTypes = {
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.node]).isRequired,
+  pageType: PropTypes.oneOf(['notes', 'twitters', 'articles']).isRequired,
 };
 export default DetailsTemplate;
--- src/templates/MainTemplate.jsx
+++ src/templates/MainTemplate.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
+import { withRouter } from 'react-router-dom';
+
+import PageContext from 'context';
 import GlobalStyle from 'theme/GlobalStyle';
 import theme from 'theme/mainTheme';
 
-const MainTemplate = ({ children }) => (
-  <div className="Root">
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
-  </div>
-);
+class MainTemplate extends Component {
+  state = { pageType: 'notes' };
+
+  componentDidMount() {
+    this.setCurrentPage();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.setCurrentPage(prevState);
+  }
+
+  setCurrentPage = (prevState = '') => {
+    const pageTypes = ['twitters', 'articles', 'notes'];
+    const {
+      location: { pathname },
+    } = this.props;
+
+    const [currentPage] = pageTypes.filter(page => pathname.includes(page));
+
+    if (prevState.pageType !== currentPage) {
+      this.setState({ pageType: currentPage });
+    }
+  };
+
+  render() {
+    const { children } = this.props;
+    const { pageType } = this.state;
+    return (
+      <div className="Root" value={pageType}>
+        <PageContext.Provider>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </PageContext.Provider>
+      </div>
+    );
+  }
+}
 
 MainTemplate.propTypes = {
   children: PropTypes.element.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }),
 };
 
-export default MainTemplate;
+MainTemplate.defaultProps = {
+  location: {
+    pathname: '/',
+  },
+};
+export default withRouter(MainTemplate);
