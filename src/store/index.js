bce02cba3bd40d2ccd8261b20e7afde1776886d0
jmargielewski <kuba.margielewski@gmail.com>
2020-03-24 22:19:40 +0000

    apply redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   rootReducer,
   /* preloadedState, */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(applyMiddleware(thunk)),
 );
 /* eslint-enable */
 
